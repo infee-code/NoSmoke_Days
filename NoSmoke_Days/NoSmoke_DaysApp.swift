7cec8b5393a417675d132b5017f25aefa7534a4f
Viktor <wanyingfeisee@outlook.com>
2025-03-09 20:47:20 +0800

    Initial Commit

---
+++ NoSmoke_Days/NoSmoke_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSmoke_DaysApp.swift
+//  NoSmoke_Days
+//
+//  Created by 万迎飞 on 2025/3/9.
+//
+
+import SwiftUI
+
+@main
+struct NoSmoke_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoSmoke_DaysTests/NoSmoke_DaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoSmoke_DaysTests.swift
+//  NoSmoke_DaysTests
+//
+//  Created by 万迎飞 on 2025/3/9.
+//
+
+import Testing
+@testable import NoSmoke_Days
+
+struct NoSmoke_DaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
